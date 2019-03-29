1fe81e67b7d85761156f8f87e01dc8ece3443bc5
Haklerz <haakon.lervik@gmail.com>
2019-03-29 17:24:31 +0100

    printing method

--- src/com/voidfall/continuance/Game.java
+++ src/com/voidfall/continuance/Game.java
@@ -5,6 +5,7 @@
  */
 package com.voidfall.continuance;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -12,53 +13,75 @@ import java.util.Scanner;
  * @author Hakerz
  */
 public class Game {
-
     private boolean running;
-    private Scanner input;
+    private final Scanner input;
+    private HashMap<String, String> text;
 
+    /**
+     *
+     */
     public Game() {
         this.input = new Scanner(System.in);
+        this.text = new HashMap<>();
+        fillText();
     }
 
+    /**
+     *
+     */
     public void run() {
         setRunning(true);
         while (isRunning()) {
-            print("cursor");
+            print("CURSOR");
             Instruction instruction = Instruction.parseInstruction(input.nextLine());
             executeInstruction(instruction);
         }
         input.close();
     }
-
-    private void setRunning(boolean running) {
-        this.running = running;
-    }
     
-    private boolean isRunning() {
-        return this.running;
-    }
-
-    private void print(String info) {
-        switch (info) {
-            case "cursor":
-                System.out.print("> ");
-                break;
-            
-            case "unknown command":
-                System.out.println("I don't know what you mean.");
-                break;
-        }
-    }
-
+    /**
+     *
+     */
     private void executeInstruction(Instruction instruction) {
         switch (instruction.getCommand()) {
             case QUIT:
                 setRunning(false);
+                print("QUITMSG");
                 break;
             
             case UNKNOWN:
-                print("unknown command");
+                print("UNKNOWNCMD");
                 break;
         }
+    }
+
+    /**
+     *
+     */
+    private void setRunning(boolean running) {
+        this.running = running;
+    }
+    
+    /**
+     *
+     */
+    private boolean isRunning() {
+        return this.running;
+    }
+    
+    /**
+     *
+     */
+    private void fillText() {
+        this.text.put("CURSOR", "> ");
+        this.text.put("UNKNOWN_CMD", "Unrecognized command\n");
+        this.text.put("QUIT_MSG", "Goodbye!\n");
+    }
+
+    /**
+     *
+     */
+    private void print(String info) {
+        System.out.print(text.get(info));
     }
 }
