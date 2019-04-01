3480eb3673c40ae0b4b0dc627838b6ec1003c3bb
Haklerz <haakon.lervik@gmail.com>
2019-04-01 09:20:28 +0200

    Location class

---
+++ src/com/voidfall/continuance/Location.java
@@ -0,0 +1,13 @@
+package com.voidfall.continuance;
+
+/**
+ * Location
+ */
+public class Location {
+    private String description;
+    private ItemCollection items;
+
+    public Location(String description) {
+        this.description = description;
+    }
+}
